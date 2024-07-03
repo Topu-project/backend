155dddf584165ae1dba00391f97dbe1fc019e837
falsystack <falsystack@gmail.com>
2024-07-04 01:03:32 +0900

    docs: Create postRecruitments

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
     java
     id("org.springframework.boot") version "3.3.0"
     id("io.spring.dependency-management") version "1.1.5"
+    id("org.asciidoctor.jvm.convert") version "3.3.2"
+
 }
 
 group = "jp.falsystack"
@@ -17,16 +19,25 @@ configurations {
     compileOnly {
         extendsFrom(configurations.annotationProcessor.get())
     }
+    asciidoctorExt
 }
 
 repositories {
     mavenCentral()
 }
 
+val asciidoctorExt = configurations.create("asciidoctorExt") {
+    extendsFrom(configurations["testImplementation"])
+}
+
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-validation")
+
+    // rest docs
+    testImplementation("org.springframework.restdocs:spring-restdocs-mockmvc")
+    asciidoctorExt("org.springframework.restdocs:spring-restdocs-asciidoctor")
 
 
     compileOnly("org.projectlombok:lombok")
