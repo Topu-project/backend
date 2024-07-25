c6dced6ee0d75bcd00dfecde4ff79457bd6aa378
falsystack <falsystack@gmail.com>
2024-07-25 21:44:30 +0900

    test: Add When write recruitment techstack and position will be registered test

--- src/test/java/jp/falsystack/backend/recruitments/usecases/RecruitmentUsecasesTest.java
+++ src/test/java/jp/falsystack/backend/recruitments/usecases/RecruitmentUsecasesTest.java
@@ -5,7 +5,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 import java.time.LocalDate;
 import jp.falsystack.backend.recruitments.entities.enums.ProgressMethods;
 import jp.falsystack.backend.recruitments.entities.enums.RecruitmentCategories;
+import jp.falsystack.backend.recruitments.repositories.PositionTagsRepository;
 import jp.falsystack.backend.recruitments.repositories.RecruitmentsRepository;
+import jp.falsystack.backend.recruitments.repositories.TechStackTagsRepository;
 import jp.falsystack.backend.recruitments.usecases.in.PostRecruitments;
 import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.DisplayName;
@@ -21,6 +23,8 @@ import org.springframework.transaction.annotation.Transactional;
 class RecruitmentUsecasesTest {
 
   private final RecruitmentsRepository repository;
+  private final TechStackTagsRepository techStacksRepository;
+  private final PositionTagsRepository positionsRepository;
   private final RecruitmentUsecases usecase;
 
   @Transactional
@@ -53,6 +57,37 @@ class RecruitmentUsecasesTest {
     assertThat(recruitments.get(0).getRecruitmentsPositionTags().get(0).getPositionTags()
         .getPositionTagName()).isEqualTo("#백엔드");
     assertThat(recruitments.get(0).getRecruitmentDeadline()).isEqualTo(LocalDate.of(2024, 10, 01));
+  }
+
+  @Test
+  @DisplayName("모집 응모글을 작성하면 기술스택과 모집 포지션이 등록된다")
+  void whenWriteRecruitmentTechStackAndPositionWillBeRegistered() {
+    // given
+    var recruitment = PostRecruitments.builder()
+        .recruitmentCategories(RecruitmentCategories.PROJECT)
+        .progressMethods(ProgressMethods.ONLINE)
+        .techStacks("#Kotlin")
+        .recruitmentPositions("#백엔드")
+        .numberOfPeople(2)
+        .progressPeriod(1)
+        .recruitmentDeadline(LocalDate.of(2024, 10, 01))
+        .contract("kotlin@gmila.com")
+        .subject("백엔드 개발자 모집합니다")
+        .content("추노하지마세요")
+        .build();
+
+    // when
+    usecase.post(recruitment);
+
+    // then
+    var positions = positionsRepository.findAll();
+    var techStacks = techStacksRepository.findAll();
+
+    assertThat(positions).hasSize(1);
+    assertThat(positions.get(0).getPositionTagName()).isEqualTo("#백엔드");
+
+    assertThat(techStacks).hasSize(1);
+    assertThat(techStacks.get(0).getTechStackTagName()).isEqualTo("#Kotlin");
   }
 
   @Transactional
